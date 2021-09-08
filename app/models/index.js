02f504061a54c2ab84be990512a4439a5f7e6ac1
Matias <jackomaster303@gmail.com>
2021-09-08 17:53:45 -0300

    Segunda etapa de implementacion del modelo de base de datos.    Se agrego la definicion del modelo:       + Issue    Se agregaron las relaciones en base de datos de los siguientes modelos:       + State       + Priority Level       + Tag       + Tag List       + Category       + Category List       + Issue

--- app/models/index.js
+++ app/models/index.js
@@ -19,6 +19,14 @@ db.ProjectGRoup = require("./project_group.models")(sequelize, Sequelize);
 db.User = require("./user.models")(sequelize, Sequelize);
 db.Role = require("./role.models")(sequelize, Sequelize);
 
+db.Issue = require("./issue.models")(sequelize, Sequelize);
+db.state = require("./state.models")(sequelize, Sequelize);
+db.priorityLevel = require("./priority_level.models")(sequelize, Sequelize);
+db.tag = require("./tag.models")(sequelize, Sequelize);
+db.tagList = require("./tag_list.models")(sequelize, Sequelize);
+db.category = require("./category.models")(sequelize, Sequelize);
+db.categoryList = require("./category_list.models")(sequelize, Sequelize);
+
 //Relaciones modelos base
 db.Project.belongsTo(db.User, {
   foreignKey: 'id_project_creator',
@@ -31,7 +39,71 @@ db.ProjectGRoup.belongsTo(db.User)
 db.ProjectGRoup.belongsTo(db.Role)
 db.ProjectGRoup.belongsTo(db.Project)
 
+//*****************************
+//* Issue Table relationships *
+//*****************************
 
-//FIN\\Modelos Base ********************************************************
+// *The project it was generated in
+db.Issue.belongsTo(db.Project)
+
+// *The user who created it
+db.Issue.belongsTo(db.User, {
+  foreignKey: 'id_user_reporter',
+} )
+
+// *The User is tasked to solve it
+db.Issue.belongsTo(db.User, {
+  foreignKey: 'id_user_assignee',
+} )
+
+// *The state is currently at
+db.Issue.belongsTo(db.state)
+
+// *The priority level it's been assigned
+db.Issue.belongsTo(db.priorityLevel)
+
+// *The categories it has
+db.Issue.hasMany(db.categoryList)
+
+// *The tags it has
+db.Issue.hasMany(db.tagList)
+
+// *Who was the last user to updated
+db.Issue.belongsTo(db.User, {
+  foreignKey: 'last_updated_by',
+} )
+
+
+//**************************************
+//* Tag & Tag_List Table relationships *
+//**************************************
+db.tag.hasMany(db.tagList)
+
+db.tagList.belongsTo(db.tag)
+db.tagList.belongsTo(db.Issue)
+
+
+//************************************************
+//* Category & Category_List Table relationships *
+//************************************************
+db.category.hasMany(db.categoryList)
+
+db.categoryList.belongsTo(db.category)
+db.categoryList.belongsTo(db.Issue)
+
+
+//*****************************
+//* State Table relationships *
+//*****************************
+db.state.hasMany(db.Issue)
+
+
+//**************************************
+//* Priority_Level Table relationships *
+//**************************************
+db.priorityLevel.hasMany(db.Issue)
+
+
+//*FIN\\Modelos Base ********************************************************
 
 module.exports = db;
---
+++ app/models/issue.models.js
@@ -0,0 +1,35 @@
+const {DataTypes} = require ('sequelize');
+
+
+module.exports = (sequelize, Sequelize) => {
+
+    const Issue = sequelize.define('Issue', {
+
+        //*> Automated or FK Fields
+
+        //* Id_Issue
+        //* Id_Project
+        //* Id_user_reporter (user id)
+        //* Id_user_assignee (user id)
+        //* Id_state
+        //* Id_priority_level
+        //* Id_category_list
+        //* Id_tag_list
+        //* Id_tag_list
+        //* createdAt
+        //* updatedAt
+        //* last_updated_by (user id)
+        description:{
+            type: DataTypes.STRING(500),
+            allowNull: false,
+        },
+        repeatability: {
+            type: DataTypes.STRING(250),
+            allowNull: false,
+        },
+
+    }, {});
+
+    return Issue;
+
+}
